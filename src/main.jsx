49ac5fb1f4c6a5b7e1cafbcf57d88f0ab019b286
Luyao Zheng <76152761+Miazhengluyao@users.noreply.github.com>
2026-05-28 00:34:27 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MoodParkWireframe from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MoodParkWireframe />
-  </React.StrictMode>
-);
